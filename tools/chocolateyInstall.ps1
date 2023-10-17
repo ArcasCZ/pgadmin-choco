25aee5da02ebf2ca68570a2b9257e6c5dcbff512
Vojtěch Olej <vojtech.olej@logex.com>
2023-10-17 12:17:50 +0200

    Take ownership + fix package

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,24 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$packageName   = 'pgadmin4'
+$toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64         = 'https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v7.7/windows/pgadmin4-7.7-x64.exe'
+$checksum64    = '11119290d41b879b051922da73d09460a144196a94c847053e09bc38398c685b'
+$checksumType64= 'sha256'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /ALLUSERS /SP-' # Inno Setup
+  validExitCodes= @(0)
+
+  softwareName  = 'pgAdmin 4'
+
+  url64         = $url64
+  checksum64    = $checksum64
+  checksumType64= $checksumType64
+}
+
+Install-ChocolateyPackage @packageArgs
