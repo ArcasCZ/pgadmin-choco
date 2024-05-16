0e1f57b4f03a3fcb70f86fab0d47aa7299cf25c0
Vojtěch Olej <Vojtech.Olej@logex.com>
2024-05-16 10:19:49 +0200

    Updated PGAdmin to 8.6.0

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName   = 'pgadmin4'
 $toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64         = 'https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v8.5/windows/pgadmin4-8.5-x64.exe'
-$checksum64    = 'dff39bd4792d10d1f0512aa2292f14ba2ddeed880b3107c477a92ae0dc2d73b2'
+$url64         = 'https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v8.6/windows/pgadmin4-8.6-x64.exe'
+$checksum64    = '8ef6bd7866665bcc6d2cc10e8c85e6016ca4a6b3ca76f8e7f7b5f349f6929c82'
 $checksumType64= 'sha256'
 
 $packageArgs = @{
