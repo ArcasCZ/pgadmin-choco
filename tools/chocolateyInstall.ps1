1bf12423363dd882808935d9b8195e78b5678a69
Vojtech Olej <Vojtech.Olej@logex.com>
2025-03-03 14:33:26 +0100

    Update version to 4.9.0

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName   = 'pgadmin4'
 $toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64         = 'https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v8.14/windows/pgadmin4-8.14-x64.exe'
-$checksum64    = '5F1BB9E6F382964ACBB90156C2F38B7800B02D77CF6934F73B826240ED004110'
+$url64         = 'https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v9.0/windows/pgadmin4-9.0-x64.exe'
+$checksum64    = '04A1A4ADF93DAAA810D80B8493DD4BC7DD3166F3B65D86F0E2985B80664FED87'
 $checksumType64= 'sha256'
 
 $packageArgs = @{
